e0ebaa8111c1cf89169e895357ba922f65514d05
Aditya Tayal <aditya.tayal@grazitti.local>
2023-04-04 18:13:40 +0530

    Second Deployment

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,11 +1,11 @@
-import { Fragment } from "react";
+import Image from "next/image";
 
 import classes from "../styles/MeetupDetail.module.css";
 
 function MeetupDetail(props) {
   return (
     <section className={classes.detail}>
-      <img src={props.image} alt={props.title} />
+      <Image src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
